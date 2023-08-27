e9c2f5bad6853a478c6bcb85384cc54bc468349e
WyattBram <143138924+WyattBram@users.noreply.github.com>
2023-08-27 19:50:07 -0400

    Update OrderList.java

--- OrderList.java
+++ OrderList.java
@@ -1,16 +1,42 @@
 public class OrderList {
-    private String[] orderList;
+    private Order[] orderList = new Order[1];
 
     public void addOrder(Order order) {
+        int position = -1;
 
+        for (int i = 0; i< orderList.length; i++) {
+            if (orderList[i] == null) {
+                position = i;
+                orderList[i] = order;
+                break;
+            }
+        }
+        if (position == -1) {
+            Order[] temp = new Order[orderList.length*2];
+            for (int c = 0; c < orderList .length; c++){
+                temp[c] = orderList[c];
+            }
+            orderList = temp;
+            temp[temp.length / 2] = order;
+
+        }
     }
 
     public void removeOrder(int id) {
-
+        for (int i = 0; i< orderList.length; i++) {
+            if (orderList[i].order_id == id) {
+                orderList[i] = null;
+            }
+        }    
     }
 
-    public void addOrder(int id) {
+    public void readyOrder(int id) {
+        for (int i = 0; i< orderList.length; i++) {
+            if (orderList[i].order_id == id) {
+                orderList[i].ready = true;
+            }
 
+        }
     }
 
     private void sortOrder() {
@@ -18,12 +44,18 @@ public class OrderList {
     }
 
     public String printOrder(int id) {
-
+        //def not working
+        for (int i = 0; i< orderList.length; i++) {
+            if (orderList[i].order_id == id) {
+                //return to string shit;
+            }
+        }
     }
 
-    public void PrintOrders() {
+    public String printOrders() {
 
     }
-
+        
 
 }
+
